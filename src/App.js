96ea406ec4b537b634b5631743157ca1e015d608
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-04-26 17:13:56 -0500

    Components added: Records and SlotMachine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Records from "./core/Records";
 import SlotMachine from "./core/SlotMachine";
 import Layout from "./layout/Layout";
 
 function App() {
   return (
     <Layout>
-      <SlotMachine />
+      <Records />
     </Layout>
   )
 }
